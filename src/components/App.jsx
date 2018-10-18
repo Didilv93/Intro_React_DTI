8a929892e2745f682221023544d9a97de2a82fcc
Didilv93 <diegossufmg@gmail.com>
2018-10-18 09:57:21 -0300

    Commit Inicial

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default Component;
